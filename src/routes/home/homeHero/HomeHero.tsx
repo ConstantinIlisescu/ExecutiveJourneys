c9e202238ee986a1a38e99982958cf1a98e1a527
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-09-12 11:55:01 +0100

    hero image another try for a netlify bug fix

--- src/routes/home/homeHero/HomeHero.tsx
+++ src/routes/home/homeHero/HomeHero.tsx
@@ -2,9 +2,13 @@ import Button from "@/components/button/Button";
 import Logo from "@/components/Logo";
 import Container from "@/components/ui/container";
 import "./HomeHero.css";
+import heroImg from "../../../../public/images/desktop/hero-img.jpg";
 const HomeHero = () => {
   return (
-    <section className="relative h-[90vh] bg-cover bg-center home-hero-image flex flex-col justify-between py-4">
+    <section
+      style={{ backgroundImage: `url(${heroImg})` }}
+      className="relative h-[90vh] bg-cover bg-center flex flex-col justify-between py-4"
+    >
       <div className="absolute inset-0 hero-overlay"></div>
       <Container className="relative z-10 ">
         <div className="flex justify-between align-middle items-center">
