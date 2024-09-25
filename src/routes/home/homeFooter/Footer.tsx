c22d95c49b55cd7b225ccc1bc2a851bceb67ff75
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-09-25 19:51:35 +0100

    updates

--- src/routes/home/homeFooter/Footer.tsx
+++ src/routes/home/homeFooter/Footer.tsx
@@ -3,6 +3,11 @@ import Container from "@/components/ui/container";
 const Footer = () => {
   return (
     <div className="bg-dark">
+      <img
+        src="images/executive-logo.jpg"
+        alt=""
+        className="w-60 rounded-3xl mx-auto mb-16 shadow-lg"
+      />
       <Container>
         <div className="h-[1px] bg-gray-600"></div>
         <div className="flex flex-col gap-4 md:flex-row justify-between py-7">
@@ -17,17 +22,29 @@ const Footer = () => {
             <span className="hidden text-light md:block">|</span>
             <a
               className="hover:opacity-50 transition-all text-call-to-action"
-              href="tel:01418832111"
+              href="tel:00447916186677"
             >
-              07555 555 555
+              07916 186 677
             </a>
           </div>
           <div className="flex gap-3">
             <span className="montserrat-medium text-light">Follow</span>
-
             <a
               className="flex items-center justify-center single-media hover:opacity-50 transition-all"
-              href="https://www.instagram.com/"
+              href="https://wa.me/00447916186677"
+              target="_blank"
+            >
+              <svg width="17.117" height="17.118" viewBox="0 0 32 32">
+                <path
+                  d="M16.001 0C7.161 0 0 7.161 0 16.001c0 2.818.739 5.474 2.048 7.784L.107 30.929c-.169.575.384 1.127.96.959l7.154-1.937A15.936 15.936 0 0016.001 32c8.84 0 16-7.161 16-15.999S24.841 0 16.001 0zm9.31 22.992c-.395 1.111-2.007 2.111-3.268 2.334-.871.147-1.989.269-5.767-1.228-4.832-2.001-7.905-6.906-8.149-7.228-.245-.322-1.943-2.589-1.943-4.94 0-2.351 1.228-3.51 1.661-3.995.434-.485.947-.611 1.265-.611.316 0 .631.003.907.016.29.014.68-.109 1.063.807.395.927 1.351 3.156 1.469 3.386.118.23.197.512.039.832-.157.321-.236.514-.474.788-.237.275-.503.614-.719.826-.241.235-.494.489-.214.963.279.473 1.236 2.036 2.654 3.297 1.83 1.627 3.356 2.137 3.85 2.372.493.236.773.196 1.063-.118.291-.315 1.228-1.425 1.556-1.918.33-.493.659-.422.988-.321.328.102 2.069.975 2.424 1.152.355.177.593.269.683.421.087.151.087.872-.309 1.983z"
+                  transform="translate(0 0)"
+                  fill="rgb(212, 175, 55)"
+                />
+              </svg>
+            </a>
+            <a
+              className="flex items-center justify-center single-media hover:opacity-50 transition-all"
+              href="https://www.instagram.com/executive_journeys"
               target="_blank"
             >
               <svg width="17.117" height="17.118" viewBox="0 0 17.117 17.118">
@@ -51,7 +68,7 @@ const Footer = () => {
 
             <a
               className=" flex items-center justify-center single-media hover:opacity-50 transition-all"
-              href="https://www.facebook.com"
+              href="https://www.facebook.com/profile.php?id=61561023843773&locale=en_GB"
               target="_blank"
             >
               <svg width="10.082" height="18.966" viewBox="0 0 10.082 18.966">
@@ -63,7 +80,7 @@ const Footer = () => {
               </svg>
             </a>
 
-            <a
+            {/* <a
               className="flex items-center justify-center single-media hover:opacity-50 transition-all"
               href="https://twitter.com/"
               target="_blank"
@@ -75,8 +92,8 @@ const Footer = () => {
                   fill="rgb(212, 175, 55)"
                 ></path>
               </svg>
-            </a>
-
+            </a> */}
+            {/* 
             <a
               className="flex items-center justify-center single-media hover:opacity-50 transition-all"
               href="https://www.linkedin.com/"
@@ -88,9 +105,9 @@ const Footer = () => {
                   d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
                 ></path>
               </svg>
-            </a>
+            </a> */}
 
-            <a
+            {/* <a
               className="flex items-center justify-center single-media hover:opacity-50 transition-all"
               href="https://www.youtube.com/"
               target="_blank"
@@ -106,7 +123,7 @@ const Footer = () => {
                   d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
                 ></path>
               </svg>
-            </a>
+            </a> */}
           </div>
         </div>
       </Container>
