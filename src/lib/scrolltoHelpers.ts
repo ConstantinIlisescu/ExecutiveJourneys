c4d4be895d7e0cbe35964993f84328803460f36b
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-10-26 18:05:06 +0100

    try a fix for smooth scroling not working on safary

--- src/lib/scrolltoHelpers.ts
+++ src/lib/scrolltoHelpers.ts
@@ -1,12 +1,22 @@
 export const scrollToHash = (pageHash: string) => {
   const targetElement = document.getElementById(pageHash);
   if (targetElement) {
-    targetElement.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+    try {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    } catch (e) {
+      // Fallback for browsers that don't support smooth scrolling
+      targetElement.scrollIntoView();
+    }
   }
 };
 export const scrollToStartPage = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  try {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  } catch (e) {
+    // Fallback for smooth scrolling support
+    window.scrollTo(0, 0);
+  }
 };
